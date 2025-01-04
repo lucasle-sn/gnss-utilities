d4ed970e0a6e8df3fdc431719fcf057f3322e7c5
lucasle-sn <lqtrung1993@gmail.com>
2025-01-04 20:18:29 +1100

    Add docs to fix doxygen

--- gnss_utilities/include/gnss_utilities/ntrip_client.h
+++ gnss_utilities/include/gnss_utilities/ntrip_client.h
@@ -6,6 +6,9 @@
 
 namespace qle {
 
+/**
+ * @brief NtripClient class
+ */
 class NtripClient {
  public:
   /**
@@ -51,25 +54,66 @@ class NtripClient {
    */
   ~NtripClient() { deinit(); }
 
+/**
+ * @brief Initialize object
+ */
   bool init() noexcept;
 
+/**
+ * @brief Deinitialize object
+ */
   void deinit() noexcept;
 
+/**
+ * @brief Main process of Ntrip client
+ */
   void run() noexcept;
 
  protected:
+ /**
+  * @brief Open an outstream
+  */
   virtual int openfd() noexcept;
 
+/**
+ * @brief Write data to file descriptor
+ * 
+ * @param fd File descriptor
+ * @param buffer Buffer data
+ * @param length Length of buffer data
+ */
   virtual ssize_t write_data(int fd, const char *buffer,
                              size_t length) noexcept;
 
  private:
+ /**
+  * @brief Create a socket to a host
+  * 
+  * @param host Host
+  * @param port Port
+  */
   int create_socket(const char *host, int port) noexcept;
 
+/**
+ * @brief Send an NTRIP request
+ * 
+ * @param sockfd Socket file descriptor
+ * @param host Ntrip host
+ * @param mountpoint Ntrip mountpoint
+ * @param username Ntrip username
+ * @param password Ntrip password
+ * @param user_agent User agent
+ */
   bool send_request(int sockfd, const char *host, const char *mountpoint,
                     const char *username, const char *password,
                     const char *user_agent) noexcept;
 
+/**
+ * @brief Forward data from socket file descriptor to a file descriptor
+ * 
+ * @param sockfd Socket file descriptor
+ * @param fd Output file descriptor
+ */
   void forward(int sockfd, int fd) noexcept;
 
   const char *host_{nullptr};        ///< NTRIP host
@@ -79,9 +123,9 @@ class NtripClient {
   const char *password_{nullptr};    ///< Password
   const char *outstream_{nullptr};   ///< Output
 
-  const char *cUserAgent_ = "NTRIP client/1.0";
-  int sockfd_{};
-  int fd_{};
+  const char *cUserAgent_ = "NTRIP client/1.0"; ///< User agent
+  int sockfd_{}; ///< Socket file descriptor
+  int fd_{}; ///< Outpur file descriptor
 };
 
 }  // namespace qle
--- gnss_utilities/src/include/gnss_utilities/internal/base64.h
+++ gnss_utilities/src/include/gnss_utilities/internal/base64.h
@@ -5,8 +5,15 @@
 
 namespace qle {
 
+/**
+ * @brief Encode data on Base64
+ *
+ * @param raw Raw datastream
+ * @param len Length of raw datastream
+ * @param out Output datastream
+ */
 void encode_base64(const char *raw, size_t len, char *out);
 
-}
+}  // namespace qle
 
-#endif // GNSS_UTILITIES_INTERNAL_BASE64_H
+#endif  // GNSS_UTILITIES_INTERNAL_BASE64_H
