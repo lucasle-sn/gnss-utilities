210a11d67391dcb7b0a06f2ea2cc906e95dbdc62
lucasle-sn <lqtrung1993@gmail.com>
2025-01-11 17:58:31 +1100

    Refactor ntrip-logging tool

--- tools/src/ntrip_logging.cc
+++ tools/src/ntrip_logging.cc
@@ -11,19 +11,21 @@
 void handle_sigint(int sig) {
   fprintf(stdout, "Caught signal %d, Ctrl + C pressed. Exiting gracefully...\n",
           sig);
-  exit(0);  // Exit the program
+  exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **args) {
   // Set up the signal handler for SIGINT
   if (signal(SIGINT, handle_sigint) == SIG_ERR) {
-    fprintf(stderr, "Unable to catch SIGINT");
-    return 1;
+    fprintf(stderr, "Unable to catch SIGINT\n");
+    return EXIT_FAILURE;
   }
 
-  if (argc < 5) {
-    fprintf(stderr, "Missing arguments");
-    return 1;
+  if (argc != 5) {
+    fprintf(stderr, "Invalid arguments\n");
+    fprintf(stderr,
+            "Usage: ntrip-logging <host> <mountpoint> <username> <password>\n");
+    return EXIT_FAILURE;
   }
 
   auto logger_cfg =
@@ -32,10 +34,12 @@ int main(int argc, char **args) {
   const char *mountpoint = args[2];
   const char *username = args[3];
   const char *password = args[4];
+
   auto ntrip = std::make_unique<qle::NtripClient>(host, 2101, mountpoint,
                                                   username, password);
+
   ntrip->init();
   ntrip->run();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
